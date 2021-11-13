d6fd9636a8b9cbf69a661aa77d05ec15aac1c746
alexYooDev <alexyoodev@alexYooDevs-MacBook-Air.local>
2021-11-13 23:58:22 +0900

    handling event according to  react event handler grammmar

--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -1,11 +1,15 @@
 function Todo(props) {
+  
+  const deletehandler = () => {
+    return alert('deleted')
+  }
   return (
     <div>
       <h1>My Todos</h1>
       <div className='card'>
         <h2>{props.text}</h2>
         <div className='actions'>
-          <button className='btn' onClick=''>Delete</button>
+          <button className='btn' onClick={deletehandler}>Delete</button>
         </div>
       </div>
     </div>
