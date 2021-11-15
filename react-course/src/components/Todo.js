a204febceb2523469c5406c413f0f933f3472f94
alexYooDev <alexyoodev@alexYooDevs-MacBook-Air.local>
2021-11-16 01:29:23 +0900

    toDoModalFunc

--- react-course/src/components/Todo.js
+++ react-course/src/components/Todo.js
@@ -1,18 +1,29 @@
+import { useState } from 'react';
+
+import Modal from './Modal.js';
+import Backdrop from './Backdrop.js';
+
 function Todo(props) {
-  
-  const deletehandler = () => {
-    return alert('deleted')
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true)
+    // one logic does different thing for each of the components
   }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
-    <div>
-      <h1>My Todos</h1>
       <div className='card'>
         <h2>{props.text}</h2>
         <div className='actions'>
-          <button className='btn' onClick={deletehandler}>Delete</button>
+          <button className='btn' onClick={deleteHandler}>Delete</button>
         </div>
+        {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
+        {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
       </div>
-    </div>
   );
 };
 
