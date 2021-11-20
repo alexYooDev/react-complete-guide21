dc6c8faefc7ccf98f05611d7acabedb8eb8ccaf8
d_flex <hiyoo95@gmail.com>
2021-11-20 22:39:31 +0900

    controlled form

--- react-basic/src/components/NewExpenses/ExpenseForm.jsx
+++ react-basic/src/components/NewExpenses/ExpenseForm.jsx
@@ -3,51 +3,94 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
 
-  // const [title, setTitle] = useState('');
-  // const [amount, setAmount] = useState('');
-  // const [date, setDate] = useState('');
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
 
-  const [userInput, setUserInput ] = useState({
-    title : '',
-    amount : '',
-    date: ''
-  })
+  // const [userInput, setUserInput ] = useState({
+  //   title : '',
+  //   amount : '',
+  //   date: ''
+  // })
 
   const titleChangeHandler = (e) => {
-    setUserInput({
-      ...userInput,
-      title: e.target.value
-    })
-  }
+    setTitle(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   title: e.target.value
+    // })
+    // setUserInput((prevState)=>{
+    //   return {...prevState, title: e.title.value};
+    // });
+  };
 
   const amountChangeHandler = (e) => {
-    setUserInput({
-      ...userInput,
-      amount: e.target.value
-    })
-  }
+    setAmount(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   amount: e.target.value
+    // })
+    // setUserInput((prevState)=>{
+    //   return {...prevState, amount: e.title.value};
+    // });
+  };
 
   const dateChangeHandler = (e) => {
-    setUserInput({
-      ...userInput,
-      date: e.target.value
-    })
+    setDate(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   date: e.target.value
+    // })
+    // setUserInput((prevState)=>{
+    //   return {...prevState, date: e.title.value};
+    // });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    
+    const expenseData = {
+      title: title,
+      amount: amount,
+      date: new Date(date)
+    };
+    
+    console.log(expenseData);
+    setTitle('');
+    setAmount('');
+    setDate('');
   }
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input 
+            type="text" 
+            value={title} 
+            onChange={titleChangeHandler} 
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler} />
+          <input 
+            type="number" 
+            min='0.01' 
+            step='0.01' 
+            value={amount} 
+            onChange={amountChangeHandler} 
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type="date" min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
+          <input 
+            type="date" 
+            min='2019-01-01' 
+            max='2022-12-31' 
+            value={date} 
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className='new-expenses__actions'>
