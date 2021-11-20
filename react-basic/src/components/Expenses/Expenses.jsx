5d58eb4f7aada86d09675ee1a33f9c9669c35ace
d_flex <hiyoo95@gmail.com>
2021-11-20 15:52:15 +0900

    components splited

---
+++ react-basic/src/components/Expenses/Expenses.jsx
@@ -0,0 +1,32 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+import Card from "../UI/Card";
+
+const Expenses = (props) => {
+  return (
+    <Card className='expenses'>
+      <ExpenseItem 
+        title={props.data[0].title} 
+        date={props.data[0].date} 
+        amount={props.data[0].amount}
+      />
+      <ExpenseItem 
+        title={props.data[1].title} 
+        date={props.data[1].date} 
+        amount={props.data[1].amount}
+      />
+      <ExpenseItem 
+        title={props.data[2].title} 
+        date={props.data[2].date} 
+        amount={props.data[2].amount}
+      />
+      <ExpenseItem 
+        title={props.data[3].title} 
+        date={props.data[3].date} 
+        amount={props.data[3].amount}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
